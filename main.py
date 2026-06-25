fc0a7ae868f842252ea64582839cca0f73e02860
its_sk625 <khansumairaax@gmail.com>
2026-06-25 15:41:36 +0530

    added metrics

--- main.py
+++ main.py
@@ -15,10 +15,11 @@ from duplicates.email_dedup import dedup_emails
 from duplicates.phone_dedup import dedup_phones
 from enrichment.risk_scoring import score_risk
 from scoring.dq_scoring import score_dq
-from metrics.calculate_metrics import calculate_metrics
+from metrics.calculate_dashboardmetrics import calculate_dashboard_metrics
 from db.connection import get_connection
 from io import StringIO
 import streamlit as st
+import uuid
 
 
 def main():
@@ -27,95 +28,112 @@ def main():
     try:
         uploaded_file=st.file_uploader("Upload CSV", type=["csv"])
         if uploaded_file:
+            file_id=str(uuid.uuid4())
             df=pd.read_csv(uploaded_file)
-            conn=get_connection()
-            cursor=conn.cursor()
-            file_id='1'
-            df["file_id"]=file_id
-            df=df[["file_id",
-                    "first_name",
-                    "last_name",
-                    "dob",
-                    "country",
-                    "gender",
-                    "email",
-                    "phone_no"]]
-            buffer = StringIO()
-            df.to_csv(buffer, index=False)
-            buffer.seek(0)
+            if st.button("Process File"):
+                with st.spinner("Processing records..."):
+                    conn=get_connection()
+                    cursor=conn.cursor()
+                    df["file_id"]=file_id
+                    df=df[["file_id",
+                            "first_name",
+                            "last_name",
+                            "dob",
+                            "country",
+                            "gender",
+                            "email",
+                            "phone_no"]]
+                    buffer = StringIO()
+                    df.to_csv(buffer, index=False)
+                    buffer.seek(0)
 
-            cursor.copy_expert(
-                """
-                COPY raw_customer_records
-                (
-                    file_id,
-                    first_name,
-                    last_name,
-                    dob,
-                    country,
-                    gender,
-                    email,
-                    phone_no
-                )
-                FROM STDIN
-                WITH CSV HEADER
-                """,
-                buffer
-            )
-            conn.commit()
-            df['extracted_country']=np.nan
-            df['extracted_operator']=np.nan
-            
-            #validation
-            df=validate_names(df)
+                    cursor.copy_expert(
+                        """
+                        COPY raw_customer_records
+                        (
+                            file_id,
+                            first_name,
+                            last_name,
+                            dob,
+                            country,
+                            gender,
+                            email,
+                            phone_no
+                        )
+                        FROM STDIN
+                        WITH CSV HEADER
+                        """,
+                        buffer
+                    )
+                    conn.commit()
+                    df['extracted_country']=np.nan
+                    df['extracted_operator']=np.nan
+                    
+                    #validation
+                    df=validate_names(df)
 
-            df=validate_dobs(df)
-            print('.')
+                    df=validate_dobs(df)
+                    print('.')
 
-            df=validate_phones(df)
-            df=validate_emails(df)
-            #standardization
-            df=standardize_names(df)
-            df=standardize_dobs(df)
-            df=standardize_emails(df)
-            df=standardize_country(df)
-            df=standardize_gender(df)
-            df=enrich_emails(df)
-            #df=enrich_phones(df)
-            df=score_risk(df)
+                    df=validate_phones(df)
+                    df=validate_emails(df)
+                    #standardization
+                    df=standardize_names(df)
+                    df=standardize_dobs(df)
+                    df=standardize_emails(df)
+                    df=standardize_country(df)
+                    df=standardize_gender(df)
+                    df=enrich_emails(df)
+                    #df=enrich_phones(df)
+                    df=score_risk(df)
 
-            df=df[['file_id','cleaned_name','cleaned_dob', 'cleaned_email','cleaned_phoneno', 'standardized_country','name_issues','dob_issues', 'email_issues','phoneno_issues','email_classified_as','extracted_domain', 'extracted_operator','extracted_country','risk_score','cleaned_gender','iso_code','nationality_issue', 'gender_issues','is_disposable_email']]
-            df['risk_score']=df["risk_score"].astype("Int64")
-            buffer=StringIO()
-            df.to_csv(buffer, index=False, header=False)
-            buffer.seek(0)
-            cursor.copy_expert(
-                """
-                COPY cleaned_customer_records(file_id,
-                    cleaned_name,
-                    cleaned_dob,cleaned_email,cleaned_phoneno,standardized_country,
-                    name_issues,dob_issues,email_issues,phoneno_issues,
-                    email_classified_as,extracted_domain, extracted_operator, extracted_country,
-                    risk_score,gender,iso_code,nationality_issue, gender_issues, is_disposable_email
-                )
-                FROM STDIN
-                WITH CSV
-                """, buffer
-                
-            )
-            conn.commit()
-            print('.')
-            df=pd.read_sql_query("select file_id,record_id,cleaned_name,cleaned_dob, cleaned_email,cleaned_phoneno, standardized_country,name_issues,dob_issues, email_issues,phoneno_issues, is_disposable_email,email_classified_as,extracted_domain, extracted_operator,extracted_country,risk_score,gender,iso_code,nationality_issue, gender_issues from cleaned_customer_records where file_id=%s", conn, params=[file_id])
-            dedup_emails(df)
-            dedup_phones(df)
+                    df=df[['file_id','cleaned_name','cleaned_dob', 'cleaned_email','cleaned_phoneno', 'standardized_country','name_issues','dob_issues', 'email_issues','phoneno_issues','email_classified_as','extracted_domain', 'extracted_operator','extracted_country','risk_score','cleaned_gender','iso_code','nationality_issue', 'gender_issues','is_disposable_email']]
+                    df['risk_score']=df["risk_score"].astype("Int64")
+                    buffer=StringIO()
+                    df.to_csv(buffer, index=False, header=False)
+                    buffer.seek(0)
+                    cursor.copy_expert(
+                        """
+                        COPY cleaned_customer_records(file_id,
+                            cleaned_name,
+                            cleaned_dob,cleaned_email,cleaned_phoneno,standardized_country,
+                            name_issues,dob_issues,email_issues,phoneno_issues,
+                            email_classified_as,extracted_domain, extracted_operator, extracted_country,
+                            risk_score,gender,iso_code,nationality_issue, gender_issues, is_disposable_email
+                        )
+                        FROM STDIN
+                        WITH CSV
+                        """, buffer
+                        
+                    )
+                    conn.commit()
+                    print('.')
+                    df=pd.read_sql_query("select file_id,record_id,cleaned_name,cleaned_dob, cleaned_email,cleaned_phoneno, standardized_country,name_issues,dob_issues, email_issues,phoneno_issues, is_disposable_email,email_classified_as,extracted_domain, extracted_operator,extracted_country,risk_score,gender,iso_code,nationality_issue, gender_issues from cleaned_customer_records where file_id=%s", conn, params=[file_id])
+                    dedup_emails(df)
+                    dedup_phones(df)
+                    
+                    score_dq()
+                    st.write("Completed!")
+                    calculate_dashboard_metrics()
+                    report=pd.read_sql_query(
+                    """select * from metrics where repo_type=%s order by snapshot_timestamp desc limit 1""", conn, params=["email"]
+                    )
+                    st.success("Processing completed sucessfully!")
+                    st.header("DQ Report")
+                    st.dataframe(report)
+                    csv=report.to_csv(index=False)
+                    st.download_button(
+                        "Download Report",
+                        csv,
+                        file_name="metrics_report.csv",
+                        mime="text/csv"
+                    )
+                    
+                    
+                    cursor.close()
+                    conn.close()
+                    
             
-            score_dq()
-            st.write("Completed!")
-            #calculate_metrics()
-            
-            
-            cursor.close()
-            conn.close()
     
     except Exception as e:
         st.error(f"Error: {e}")
--- metrics/calculate_dashboardmetrics.py
+++ metrics/calculate_dashboardmetrics.py
@@ -4,42 +4,44 @@ from db.connection import get_connection
 def calculate_dashboard_metrics():
     conn=get_connection()
     cursor=conn.cursor()
-    cursor.execute(f"""select count(*) from master_customer_email""", conn)
+    cursor.execute(f"""select count(*) from final_customer_email""", conn)
     total=cursor.fetchone()[0]
-    cursor.execute(f"""select count(*) from master_customer_email where cleaned_dob is null""", conn)
-    invalid_dob_counts=cursor.fetchone()[0]
-    invalid_dob_pct=0 if (total==0) else  (invalid_dob_counts/total)*100
-    cursor.execute(f"""select count(*) from master_customer_email where cleaned_phoneno is null""", conn)
-    invalid_phoneno_counts=cursor.fetchone()[0]
-    invalid_phoneno_pct=0 if (total==0) else (invalid_phoneno_counts/total)*100
-    query=f"""SELECT count(*) FROM master_customer_email where  standardized_country is null;"""
+    cursor.execute(f"""select count(*) from final_customer_email where cleaned_name is not null""", conn)
+    valid_name_counts=cursor.fetchone()[0]
+    cursor.execute(f"""select count(*) from final_customer_email where cleaned_dob is not null""", conn)
+    valid_dob_counts=cursor.fetchone()[0]
+    cursor.execute(f"""select count(*) from final_customer_email where cleaned_phoneno is not null""", conn)
+    valid_phoneno_counts=cursor.fetchone()[0]
+    query=f"""SELECT count(*) FROM final_customer_email where  standardized_country is not null;"""
     cursor.execute(query, conn)
-    invalid_country_counts = cursor.fetchone()[0]
-    invalid_country_pct=0 if (total==0) else  (invalid_country_counts/total)*100
-    cursor.execute(f"""select avg(dq_score) from master_customer_email""", conn)
+    valid_country_counts = cursor.fetchone()[0]
+    cursor.execute(f"""select count(*) from final_customer_email where gender is not null""", conn)
+    valid_gender_counts=cursor.fetchone()[0]
+    cursor.execute(f"""select avg(dq_score) from final_customer_email""", conn)
     dq_avg=cursor.fetchone()[0]
-    cursor.execute(f"""insert into dashboard_metrics (total_records, invalid_dob_pct, invalid_dob_counts, invalid_phoneno_pct, invalid_phoneno_counts, invalid_country_pct, invalid_country_counts,
-                   dq_score, repo_type) values (%s,%s,%s,%s, %s, %s, %s, %s, %s)""", (total, invalid_dob_pct, invalid_dob_counts, invalid_phoneno_pct, invalid_phoneno_counts, invalid_country_pct, invalid_country_counts,dq_avg, "email"))
+    cursor.execute(f"""insert into dashboard_metrics (repo_type,total_records, valid_name_count, valid_dob_count, valid_phoneno_count, valid_country_count, valid_gender_count,
+                   average_dq_score) values (%s,%s,%s,%s, %s, %s, %s, %s)""", ("email",total, valid_name_counts, valid_dob_counts,valid_phoneno_counts, valid_country_counts, valid_gender_counts,dq_avg))
     conn.commit()
-
-    cursor.execute(f"""select count(*) from master_customer_phone""", conn)
+    cursor.close()
+    conn.close()
+'''
+    cursor.execute(f"""select count(*) from final_customer_phone""", conn)
     total=cursor.fetchone()[0]
-    cursor.execute(f"""select count(*) from master_customer_phone where cleaned_dob is null""", conn)
+    cursor.execute(f"""select count(*) from final_customer_phone where cleaned_dob is null""", conn)
     invalid_dob_counts=cursor.fetchone()[0]
     invalid_dob_pct= 0 if (total==0) else (invalid_dob_counts/total)*100
-    cursor.execute(f"""select count(*) from master_customer_phone where cleaned_email is null""", conn)
+    cursor.execute(f"""select count(*) from final_customer_phone where cleaned_email is null""", conn)
     invalid_email_counts=cursor.fetchone()[0]
     invalid_email_pct= 0 if (total==0) else (invalid_email_counts/total)*100
-    query=f"""SELECT count(*) FROM master_customer_phone where standardized_country is null;"""
+    query=f"""SELECT count(*) FROM final_customer_phone where standardized_country is null;"""
     cursor.execute(query, conn)
     invalid_country_counts = cursor.fetchone()[0]
     invalid_country_pct= 0 if (total==0) else (invalid_country_counts/total)*100
-    cursor.execute(f"""select avg(dq_score) from master_customer_phone""", conn)
+    cursor.execute(f"""select avg(dq_score) from final_customer_phone""", conn)
     dq_avg=cursor.fetchone()[0] or 0
     cursor.execute(f"""insert into dashboard_metrics (total_records, invalid_dob_pct, invalid_dob_counts, invalid_email_pct, invalid_email_counts, invalid_country_pct, invalid_country_counts,
                    dq_score, repo_type) values (%s,%s,%s,%s, %s, %s, %s, %s, %s)""", (total, invalid_dob_pct, invalid_dob_counts, invalid_email_pct, invalid_email_counts, invalid_country_pct, invalid_country_counts,dq_avg, "phoneno"))
     
     
-    conn.commit()
-    cursor.close()
-    conn.close()
+    conn.commit()'''
+    
