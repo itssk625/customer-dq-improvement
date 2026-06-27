b1e28e53c02af5a41aade0b8f00c30e60676665f
its_sk625 <khansumairaax@gmail.com>
2026-06-27 12:30:05 +0530

    dashboard monthly features

--- metrics/visualization.py
+++ metrics/visualization.py
@@ -176,25 +176,8 @@ def display_report_metrics(rec):
 def display_dashboard_metrics(rec):
     
     total=rec["total_records"]
-    col1, col2, col3=st.columns(3)
-    with col1:
-        circ_progress("Name", rec["valid_name_count"],total,rec["repo_type"])
-    with col2:
-        circ_progress("DOB", rec["valid_dob_count"], total,rec["repo_type"])
-    with col3:
-        if rec["repo_type"]=="email":
-            circ_progress("Phone number", rec["valid_phoneno_count"], total,rec["repo_type"])
-        else:
-            circ_progress("Email", rec["valid_email_count"], total,rec["repo_type"])
     
-    col1, col2=st.columns(2)
-    with col1:
-        circ_progress("Country", rec["valid_country_count"], total,rec["repo_type"])
-    with col2:
-        circ_progress("Gender", rec["valid_gender_count"], total,rec["repo_type"])
-        
-    st.divider()
-    
+    st.subheader("Data Quality Overview")
     col1, col2, col3=st.columns(3)
     
     with col1:
@@ -217,3 +200,23 @@ def display_dashboard_metrics(rec):
                 f"{rec['disposable_email_pct']:.2f}%"
             )
         
+    st.divider()
+    st.subheader("Attribute Validity")
+    col1, col2, col3=st.columns(3)
+    with col1:
+        circ_progress("Name", rec["valid_name_count"],total,rec["repo_type"])
+    with col2:
+        circ_progress("DOB", rec["valid_dob_count"], total,rec["repo_type"])
+    with col3:
+        if rec["repo_type"]=="email":
+            circ_progress("Phone number", rec["valid_phoneno_count"], total,rec["repo_type"])
+        else:
+            circ_progress("Email", rec["valid_email_count"], total,rec["repo_type"])
+    
+    col1, col2=st.columns(2)
+    with col1:
+        circ_progress("Country", rec["valid_country_count"], total,rec["repo_type"])
+    with col2:
+        circ_progress("Gender", rec["valid_gender_count"], total,rec["repo_type"])
+            
+    
