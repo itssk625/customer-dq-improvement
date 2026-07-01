a732f554598363517b4925b1e41199818edda505
its_sk625 <khansumairaax@gmail.com>
2026-07-01 11:51:39 +0530

    upload_date included

--- metrics/calculate_metrics.py
+++ metrics/calculate_metrics.py
@@ -20,7 +20,7 @@ def calculate_metrics():
     valid_gender_counts=cursor.fetchone()[0]
     cursor.execute(f"""select avg(dq_score) from final_customer_email""")
     dq_avg=cursor.fetchone()[0]
-    cursor.execute(f"""select upload_date from final_customer_email""")
+    cursor.execute(f"""select max(upload_date) from final_customer_email""")
     mdate=cursor.fetchone()[0]
     year=mdate.year
     month=mdate.month
@@ -48,14 +48,14 @@ def calculate_metrics():
     disposable_pct=0 if (disposable_counts==0) else (disposable_counts/total)*100
     cursor.execute(f"""select avg(dq_score) from final_customer_phone""")
     dq_avg=cursor.fetchone()[0]
-    cursor.execute(f"""select upload_date from final_customer_email""")
+    cursor.execute(f"""select max(upload_date) from final_customer_phone""")
     mdate=cursor.fetchone()[0]
     year=mdate.year
     month=mdate.month
     last_day=monthrange(year, month)[1]
     snapshot_timestamp=mdate.replace(day=last_day)
     cursor.execute(f"""insert into metrics (snapshot_timestamp,repo_type,total_records, valid_name_count, valid_dob_count, valid_email_count, valid_country_count, valid_gender_count, disposable_email_pct,
-                   average_dq_score) values (%s,%s,%s,%s,%s, %s, %s, %s, %s, %s)""", (snapshot_timestamp"phone",total, valid_name_counts, valid_dob_counts,valid_email_counts, valid_country_counts, valid_gender_counts,disposable_pct,dq_avg))
+                   average_dq_score) values (%s,%s,%s,%s,%s, %s, %s, %s, %s, %s)""", (snapshot_timestamp,"phone",total, valid_name_counts, valid_dob_counts,valid_email_counts, valid_country_counts, valid_gender_counts,disposable_pct,dq_avg))
     conn.commit()
     cursor.close()
     conn.close()
